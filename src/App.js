9727b347647354f91beaf55668bf08a6f83f8442
rprice000 <rtprice21@gmail.com>
2022-03-28 13:09:37 -0500

    fixing navigation links part 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 
 // Imports from Pages folder
-// import About from './pages/About/About';
-// import Portfolio from './pages/Portfolio/Portfolio';
-// import Resume from './pages/Resume/Resume';
-// import Contact from './pages/Contact/Contact';
+import About from '../src/pages/About/About';
+import Portfolio from '../src/pages/Portfolio/Portfolio';
+import Resume from '../src/pages/Resume/Resume';
+import Contact from '../src/pages/Contact/Contact';
 
 //Imports from Components folder
-import NavigatePortfolio from './components/Navigation/NavigatePortfolio';
-import Footer from './components/Footer/Footer'
+import Navigation from '../src/components/Navigation/Navigation';
+import Footer from '../src/components/Footer/Footer'
 
 
 
@@ -18,25 +18,22 @@ import Footer from './components/Footer/Footer'
 const App = () => {
   return (
 
-    // <Router>
-    //   <Navigation />
-    //   <main>
+    <Router>
       
-    //     <Routes>
-    //       <Route exact path="/" element={<About />} />
-    //       <Route exact path="/about" element={<About />} />
-    //       <Route exact path="/portfolio" element={<Portfolio />} />
-    //       <Route exact path="/resume" element={<Resume />} />
-    //       <Route exact path="/contact" element={<Contact />} />
-    //       <Route path="*" element={<Navigate to="/" />} />
-    //     </Routes>
-    //   </main>
-    //   <Footer />
-    // </Router>
-    <div>
-      <NavigatePortfolio></NavigatePortfolio>
-      <Footer></Footer>
-    </div>
+      <Navigation />
+      <main>
+      
+        <Routes>
+          <Route exact path="/" element={<About />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/portfolio" element={<Portfolio />} />
+          <Route exact path="/resume" element={<Resume />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   
   );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+
 
 function NavLinks(props) {
   const tabs = [ 'About', 'Portfolio', 'Resume', 'Contact'];
 
   return (
 
-      <Navbar expand="lg">
+     
 
-        <Navbar.Toggle />
-        <Navbar.Collapse className='justify-content-end pe-5'>
-        <Nav variant="pills" defaultActiveKey="/about">
+     <div>
+       <ul>
         {tabs.map((tab) => (
-          <Nav.Item className='ps-5 pe-5' key={tab}>
-            <Nav.Link
+          <li key={tab}>
+            <a
               href={"#" + tab.toLowerCase()}
               onClick={() => props.handlePageChange(tab)}
               className={
@@ -21,13 +20,12 @@ function NavLinks(props) {
               }
             >
               {tab}
-            </Nav.Link>
-          </Nav.Item>
+            </a>
+          </li>
         ))}
-        </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
+        </ul>
+        </div>
+    
   );
 
 
